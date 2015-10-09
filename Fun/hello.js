48e868f7a765e4f553fb93b4363c1345de638a8d
kzkaed <tree@scutigera.com>
2015-10-08 21:08:21 -0500

    update js

--- Fun/hello.js
+++ Fun/hello.js
@@ -1,12 +1,4 @@
-
-
 function nameAlert (name){
   alert(name);
 }
 
-function moveName (nameClass){
-  var divs = document.getElementsByTagName("div");
-  var nameById = document.getElementById("name");
-  alert(divs);
-  alert(nameById);
-}
